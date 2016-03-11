bcf5569521602666331810bb9109a493b3499d85
ncon5910 <nicholas.conner@gmail.com>
2016-03-10 20:11:00 -0500

    Initial commit

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour 
+{
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count (5,9);
+	public Count foodCount = new Count (1, 5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outerWallTiles;
+
+	private Transform boardHolder;
+	private List <Vector3> gridPositions = new List<Vector3>();
+
+	void InitialiseList()
+	{
+		gridPositions.Clear ();
+
+		for (int x = 1; x < columns - 1; x++) {
+			for (int y = 1; y < rows - 1; y++) {
+				gridPositions.Add (new Vector3 (x, y, 0f));
+			}
+		}	
+	}
+
+	void BoardSetup () {
+		boardHolder = new GameObject ("Board").transform;
+
+		for (int x = -1; x < columns + 1; x++) {
+			for (int y = -1; y < rows + 1; y++) {
+				GameObject toInstantiate = floorTiles [Random.Range (0, floorTiles.Length)];
+				if (x == -1 || x == columns || y == -1 || y == rows)
+					toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
+
+				GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+
+				instance.transform.Set
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
