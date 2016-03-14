1d67cb22d4b9e0a143c0d6042f1ed774601fd604
ncon5910 <nicholas.conner@gmail.com>
2016-03-14 14:47:31 -0400

    Did another 1.5 videos

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -54,18 +54,36 @@ public class BoardManager : MonoBehaviour
 
 				GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
 
-				instance.transform.Set
+				instance.transform.SetParent (boardHolder);
 			}
 		}
 	}
 
-	// Use this for initialization
-	void Start () {
-	
+	Vector3 RandomPosition() {
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions [randomIndex];
+		gridPositions.RemoveAt (randomIndex);
+		return randomPosition;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum) {
+		int objectCount = Random.Range (minimum, maximum + 1);
+
+		for (int i = 0; i < objectCount; i++) {
+			Vector3 randomPosition = RandomPosition ();
+			GameObject tileChoice = tileArray [Random.Range (0, tileArray.Length)];
+			Instantiate (tileChoice, randomPosition, Quaternion.identity);
+		}
 	}
+
+	public void SetupScene(int level) {
+		BoardSetup ();
+		InitialiseList ();
+		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
+		int enemyCount = (int)Mathf.Log (level, 2f);
+		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+		Instantiate(exit, new Vector3(columns -1, rows -1, 0f), Quaternion.identity);
+	}
+
 }
